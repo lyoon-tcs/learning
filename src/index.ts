dc8407c4a784719a69e96e3dda3c339ba7d31e44
Lawrence Yoon <lyoon@truechoice.io>
2022-02-01 15:36:06 -0800

    feat: pass object as arg, use proper ts type

--- src/index.ts
+++ src/index.ts
@@ -70,17 +70,17 @@ export function logPerson(person: Person) {
   console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
 
-export function filterUsers(persons: Person[], criteria: number) {
+export function filterUsers(persons: Person[], criteria: { age: number }) {
   const users = persons.filter(isUser);
   const final = users.filter((user) => {
-    return user.age === criteria;
+    return user.age === criteria.age;
   });
   // what in the world is going on here???
   return final;
 }
 
 console.log('Users of age 23:');
-filterUsers(persons, 23).forEach(logPerson);
+filterUsers(persons, { age: 23 }).forEach(logPerson);
 
 // Users of age 23:
 //  - Kate Müller, 23, Astronaut
