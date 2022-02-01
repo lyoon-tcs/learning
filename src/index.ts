505b1bf876b0399ee4d606f154f431c8b53ee763
Lawrence Yoon <lawrenceyoon@LawrenceMBP.lan>
2022-02-01 15:12:11 -0800

    feat: add #5 typescript exercise, breaking it down

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,86 @@
-function printCoord(pnt: { x: number; y: number }) {
-  console.log(`The coordinate's x value is ${pnt.x}`);
-  console.log(`The coordinate's y value is ${pnt.y}`);
+interface User {
+  type: 'user';
+  name: string;
+  age: number;
+  occupation: string;
 }
 
-printCoord({ x: 3, y: 7 });
+interface Admin {
+  type: 'admin';
+  name: string;
+  age: number;
+  role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+  {
+    type: 'user',
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep'
+  },
+  {
+    type: 'admin',
+    name: 'Jane Doe',
+    age: 32,
+    role: 'Administrator'
+  },
+  {
+    type: 'user',
+    name: 'Kate Müller',
+    age: 23,
+    occupation: 'Astronaut'
+  },
+  {
+    type: 'admin',
+    name: 'Bruce Willis',
+    age: 64,
+    role: 'World saver'
+  },
+  {
+    type: 'user',
+    name: 'Wilson',
+    age: 23,
+    occupation: 'Ball'
+  },
+  {
+    type: 'admin',
+    name: 'Agent Smith',
+    age: 23,
+    role: 'Administrator'
+  }
+];
+
+export const isAdmin = (person: Person): person is Admin =>
+  person.type === 'admin';
+
+export const isUser = (person: Person): person is User =>
+  person.type === 'user';
+
+export function logPerson(person: Person) {
+  let additionalInformation = '';
+  if (isAdmin(person)) {
+    additionalInformation = person.role;
+  }
+  if (isUser(person)) {
+    additionalInformation = person.occupation;
+  }
+  console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+export function filterUsers(persons: Person[], criteria: number) {
+  const asdf = persons.filter(isUser);
+  const final = asdf.filter((correct) => {
+    return correct.age === criteria;
+  });
+  return final;
+}
+
+console.log('Users of age 23:');
+filterUsers(persons, 23).forEach(logPerson);
+
+// Users of age 23:
+//  - Kate Müller, 23, Astronaut
+//  - Wilson, 23, Ball
