d7061055ce565f47695901d36a520ea160832eee
Lawrence Yoon <lyoon@truechoice.io>
2022-02-01 16:15:27 -0800

    feat: set up boilerplate for #6

--- src/index.ts
+++ src/index.ts
@@ -21,67 +21,9 @@ export const persons: Person[] = [
     age: 25,
     occupation: 'Chimney sweep'
   },
-  {
-    type: 'admin',
-    name: 'Jane Doe',
-    age: 32,
-    role: 'Administrator'
-  },
-  {
-    type: 'user',
-    name: 'Kate Müller',
-    age: 23,
-    occupation: 'Astronaut'
-  },
-  {
-    type: 'admin',
-    name: 'Bruce Willis',
-    age: 64,
-    role: 'World saver'
-  },
-  {
-    type: 'user',
-    name: 'Wilson',
-    age: 23,
-    occupation: 'Ball'
-  },
-  {
-    type: 'admin',
-    name: 'Agent Smith',
-    age: 23,
-    role: 'Administrator'
-  }
+  { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+  { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+  { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
+  { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
+  { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
 ];
-
-export const isAdmin = (person: Person): person is Admin =>
-  person.type === 'admin';
-
-export const isUser = (person: Person): person is User =>
-  person.type === 'user';
-
-export function logPerson(person: Person) {
-  let additionalInformation = '';
-  if (isAdmin(person)) {
-    additionalInformation = person.role;
-  }
-  if (isUser(person)) {
-    additionalInformation = person.occupation;
-  }
-  console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
-}
-
-export function filterUsers(persons: Person[], criteria: Partial<User>) {
-  const users = persons.filter(isUser);
-  const final = users.filter((user) => {
-    return user.age === criteria.age;
-  });
-  // Self note - dunno why they did it that way - so confusing. this way is way easier for me to grasp
-  return final;
-}
-
-console.log('Users of age 23:');
-filterUsers(persons, { age: 23 }).forEach(logPerson);
-
-// Users of age 23:
-//  - Kate Müller, 23, Astronaut
-//  - Wilson, 23, Ball
