4511c82f85715857d82532dbea182e4797374371
Lawrence Yoon <lyoon@truechoice.io>
2022-02-03 14:21:44 -0800

    test: add multiplication and division tests

--- src/index.ts
+++ src/index.ts
@@ -1,117 +1,12 @@
-/* ----- QUESTION 5 ----- */
-// interface User {
-//   type: 'user';
-//   name: string;
-//   age: number;
-//   occupation: string;
+// function asdf(a: unknown, b: unknown) {
+//   console.log(a);
+//   return a + b;
 // }
 
-// interface Admin {
-//   type: 'admin';
-//   name: string;
-//   age: number;
-//   role: string;
-// }
+// console.log(asdf(2, 4));
 
-// export type Person = User | Admin;
+let greet: Function;
 
-// export const persons: Person[] = [
-//   {
-//     type: 'user',
-//     name: 'Max Mustermann',
-//     age: 25,
-//     occupation: 'Chimney sweep'
-//   },
-//   {
-//     type: 'admin',
-//     name: 'Jane Doe',
-//     age: 32,
-//     role: 'Administrator'
-//   },
-//   {
-//     type: 'user',
-//     name: 'Kate Müller',
-//     age: 23,
-//     occupation: 'Astronaut'
-//   },
-//   {
-//     type: 'admin',
-//     name: 'Bruce Willis',
-//     age: 64,
-//     role: 'World saver'
-//   },
-//   {
-//     type: 'user',
-//     name: 'Wilson',
-//     age: 23,
-//     occupation: 'Ball'
-//   },
-//   {
-//     type: 'admin',
-//     name: 'Agent Smith',
-//     age: 23,
-//     role: 'Administrator'
-//   }
-// ];
-
-// export const isAdmin = (person: Person): person is Admin =>
-//   person.type === 'admin';
-// export const isUser = (person: Person): person is User =>
-//   person.type === 'user';
-
-// export function logPerson(person: Person): void {
-//   let additionalInformation = '';
-//   if (isAdmin(person)) {
-//     additionalInformation = person.role;
-//   }
-//   if (isUser(person)) {
-//     additionalInformation = person.occupation;
-//   }
-//   console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
-// }
-
-// export function filterUsers(persons: Person[], criteria: Partial<User>) {
-//   const users = persons.filter(isUser);
-//   const final = users.filter((user) => {
-//     return user.age === criteria.age;
-//   });
-//   // what in the world is going on here???
-//   // want to return an array of objects with matching CRITERIA and USER
-//   return final;
-// }
-
-// console.log('Users of age 23:');
-// filterUsers(persons, {
-//   age: 23
-// }).forEach(logPerson);
-
-/* ----- QUESTION 6 ----- */
-interface User {
-  type: 'user';
-  name: string;
-  age: number;
-  occupation: string;
-}
-
-interface Admin {
-  type: 'admin';
-  name: string;
-  age: number;
-  role: string;
-}
-
-export type Person = User | Admin;
-
-export const persons: Person[] = [
-  {
-    type: 'user',
-    name: 'Max Mustermann',
-    age: 25,
-    occupation: 'Chimney sweep'
-  },
-  { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
-  { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
-  { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
-  { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
-  { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
-];
+greet = function () {
+  console.log('asdf');
+};
